14f69186e7f4a9d3b876dfd72be7498371fcc7dd
JAIME_CARRIEL <jaimecarriel302@gmail.com>
2025-06-01 16:52:19 -0500

    Production updated

--- src/main.py
+++ src/main.py
@@ -2,6 +2,7 @@ import os
 import shutil
 import stat
 from block import markdown_to_html_node
+import sys
 
 def on_rm_error(func, path, exc_info):
     # Cambia permisos y vuelve a intentar
@@ -13,13 +14,13 @@ def static_to_public():
     #
     BASE_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
     STATIC_DIR= os.path.join(BASE_DIR, "static")
-    PUBLIC_DIR= os.path.join(BASE_DIR, "public")
+    PUBLIC_DIR= os.path.join(BASE_DIR, "docs")
     #
     if os.path.exists(PUBLIC_DIR):
         shutil.rmtree(PUBLIC_DIR, onexc=on_rm_error)
         #
-        os.mkdir(PUBLIC_DIR)    
     #
+    os.mkdir(PUBLIC_DIR)    
     copy_static_content(STATIC_DIR,PUBLIC_DIR)
 
 
@@ -45,7 +46,7 @@ def extract_title(markdown):
     raise Exception("There is no main header: h1")
 
 
-def generate_page(from_path, template_path, dest_path):
+def generate_page(from_path, template_path, dest_path, basepath):
     
     print(f"Generating from {from_path} to {dest_path} using {template_path}")
 
@@ -60,27 +61,31 @@ def generate_page(from_path, template_path, dest_path):
     
     template_file = template_file.replace("{{ Title }}", html_title)
     template_file = template_file.replace("{{ Content }}", html_node.to_html())
-    
+    template_file = template_file.replace('href="/',f'href="{basepath}')
+    template_file = template_file.replace('src="/',f'src="{basepath}')
+
     os.makedirs(os.path.dirname(dest_path), exist_ok=True)
     
     with open(dest_path, "w") as file:
         file.write(template_file)
 
     
-def generate_page_recursively(dir_path_content, template_path, dest_dir_path):
+def generate_page_recursively(dir_path_content, template_path, dest_dir_path, basepath):
     dir_entries = os.listdir(dir_path_content)
 
     for entry in dir_entries:
         if os.path.isfile(os.path.join(dir_path_content,entry)):
-            generate_page(os.path.join(dir_path_content,entry),template_path,os.path.join(dest_dir_path,entry.replace(".md",".html")))
+            generate_page(os.path.join(dir_path_content,entry),template_path,os.path.join(dest_dir_path,entry.replace(".md",".html")),basepath)
         else:
-            generate_page_recursively(os.path.join(dir_path_content,entry),template_path,os.path.join(dest_dir_path,entry))
+            generate_page_recursively(os.path.join(dir_path_content,entry),template_path,os.path.join(dest_dir_path,entry), basepath)
     return 
 
 def main():
+    BASE_PATH = f"{sys.argv[1]}/" if len(sys.argv) > 1 else "/"
+    print(BASE_PATH)
     static_to_public()
     # generate_page("content/index.md","template.html","public/index.html")
-    generate_page_recursively("content/","template.html","public/")
+    generate_page_recursively("content/","template.html","docs/", BASE_PATH)
 
 if __name__ == "__main__":
     main()
